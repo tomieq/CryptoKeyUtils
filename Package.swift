e954dc4af5e70dfbec2d53be91fe388262a471bd
tomieq <tomieq@wp.pl>
2026-04-28 13:06:00 +0200

    Upgrade SwiftExtensions to 2.0.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["CryptoKeyUtils"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tomieq/SwiftExtensions", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/tomieq/SwiftExtensions", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/tomieq/SwiftyTLV", .upToNextMajor(from: "1.0.2"))
     ],
     targets: [
